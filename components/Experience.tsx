5dd21ebd968c7f1a90e52d76f780a4725e24d58b
Raj Gupta <100614378+Raj-Gupta-au46@users.noreply.github.com>
2025-06-10 17:08:51 +0530

    update

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -84,7 +84,7 @@ const Experience = () => {
             JavaScript
           </div>
           <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Typescript
+            Docker
           </div>
           <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
             MongoDB
